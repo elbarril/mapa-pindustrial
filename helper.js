7cd42ed0afdc8e6dc1037a351b2aaf66c4fb1dde
eldaroo <darioegea@gmail.com>
2020-09-26 03:51:11 -0300

    add show var to helper in order to have diferent escenarys

--- helper.js
+++ helper.js
@@ -1,36 +1,50 @@
+const SHOW_COMPANY = "company";
+const SHOW_EMPTY = "empty";
+const SHOW_ALL = "all";
+
 var lat = -32.496;
 var lng = -58.305;
 var zoom = 14;
+var show;
 
+getShow = function() {
+    try {
+        var url_string = (window.location.href).toLowerCase();
+        var url = new URL(url_string);
+        var show = url.searchParams.get("show");
+    } catch (err) {
+        console.log("Issues with Parsing URL Parameter's - " + err);
+    }
+    this.show = show;
+    return show;
+}
 window.onload = function() {
-  try {
-    var url_string = (window.location.href).toLowerCase();
-    var url = new URL(url_string);
-    var lote = url.searchParams.get("lote");
-    var zoom = url.searchParams.get("zoom") || 14;
-  } catch (err) {
-    console.log("Issues with Parsing URL Parameter's - " + err);
-  }
+    try {
+        var url_string = (window.location.href).toLowerCase();
+        var url = new URL(url_string);
+        var lote = url.searchParams.get("lote");
+        var zoom = url.searchParams.get("zoom") || 14;
+    } catch (err) {
+        console.log("Issues with Parsing URL Parameter's - " + err);
+    }
 
-  switch (lote) {
-    case "1":
-        lat = -32.4896559;
-        lng = -58.3071581;
-    break;
-    case "2":
-      lat = -32.4937168;
-      lng = -58.3073535;
-    break;
-    case "3":
-      lat = -32.4983204;
-      lng = -58.3110433;
-    break;
-    default:
-      lat = -32.496;
-      lng = -58.305;
-      break;
-  }
-  console.log("latitud: " + lat + ", longitud: " + lng + ", zoom: "+zoom);
-
-    map.setView([lat,lng],zoom);
+    switch (lote) {
+        case "1":
+            lat = -32.4896559;
+            lng = -58.3071581;
+            break;
+        case "2":
+            lat = -32.4937168;
+            lng = -58.3073535;
+            break;
+        case "3":
+            lat = -32.4983204;
+            lng = -58.3110433;
+            break;
+        default:
+            lat = -32.496;
+            lng = -58.305;
+            break;
+    }
+    map.setView([lat, lng], zoom);
 }
